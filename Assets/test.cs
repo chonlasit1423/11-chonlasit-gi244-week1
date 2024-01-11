ca7ae4b791340a35b9db4a77aef80f1d74d90b5a
chonlasit1423 <chonlasit.teer@bumail.net>
2024-01-11 10:52:43 +0700

    script inventory

--- Assets/test.cs
+++ Assets/test.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //inventory array
+    private string[] inventoryarray = new[] {"Sword","Shield","Potion"};
+    
+    //move element in the array to list
+    private List<string> inventoryList;
+
+        // Start is called before the first frame update
     void Start()
     {
-        
+        inventoryList = new List<string>(inventoryarray);
+        DisplayInventory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inventoryList.Add("Helmet");
+            DisplayInventory();
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            inventoryList.Remove("Sword");
+            DisplayInventory();
+        }
+    }
+
+    void DisplayInventory()
+    {
+        int i = 1;
+        Debug.Log($"Inventory: {inventoryList}");
+        foreach (var Item in inventoryList)
+        {
+            Debug.Log($"{i}.{Item}");
+            i++;
+        }
     }
 }
